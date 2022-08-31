43f950b8487963e227abb7498bf4adf3cb1d26a2
TaylorJMorgan <TaylorJMorgan@users.noreply.github.com>
2022-08-31 16:37:01 +0000

    automated commit by submit50

--- frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from '../components/SignInForm';
-
-function Register() {
-    return (
-        <SignInForm title='Register' button='Register' route='/register' isRegister='true' />
-    );
-}
-
-export default Register;
